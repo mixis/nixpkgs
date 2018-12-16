24f5cbae4d1205151fb9b5d69fa2f456a0c17a94
Michael Weiss <mixis@filts.net>
2018-12-16 19:57:13 +0100

    bcachefs-tools: 2018-10-12 -> 2018-12-12

--- pkgs/tools/filesystems/bcachefs-tools/default.nix
+++ pkgs/tools/filesystems/bcachefs-tools/default.nix
@@ -2,19 +2,19 @@
 , liburcu, zlib, libaio, zstd, lz4 }:
 
 stdenv.mkDerivation rec {
-  name = "bcachefs-tools-unstable-2018-10-12";
+  name = "bcachefs-tools-unstable-2018-12-12";
 
   src = fetchgit {
     url = "https://evilpiepirate.org/git/bcachefs-tools.git";
-    rev = "55fbb25501330038e1714905b9ddeb25d875c11c";
-    sha256 = "0cwzbyf133jc0fkc8nmjcvv3wmglqhyxda1hh10hgxrbq5vm39wx";
+    rev = "a10a41fa2b1a917b0f3b34d20175867f968b2d12";
+    sha256 = "1f8pjv7jcx8iixm326xdr6g9c4z57kiwy74f4cf5bj9rbxzad81b";
   };
 
   enableParallelBuilding = true;
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ attr libuuid libscrypt libsodium keyutils liburcu zlib libaio zstd lz4 ];
   installFlags = [ "PREFIX=$(out)" ];
-  
+
   preInstall = ''
     sed -i \
       "s,INITRAMFS_DIR=/etc/initramfs-tools,INITRAMFS_DIR=$out/etc/initramfs-tools,g" Makefile
